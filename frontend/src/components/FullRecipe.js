7d0c4d9cb0630a6a61b1b7bf7e679ade231f5ef6
Waqas Fareed <fareedwaqas62@gmail.com>
2024-01-08 22:47:10 +0500

    redndred all data on full page

--- frontend/src/components/FullRecipe.js
+++ frontend/src/components/FullRecipe.js
@@ -3,7 +3,7 @@ import Link, { useParams } from "react-router-dom";
 import Data from "../data.json";
 const FullRecipe = () => {
   const { id } = useParams();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   useEffect(() => {
     const recipeData = Data.find((recipe) => {
       return recipe.id === parseInt(id);
@@ -12,17 +12,17 @@ const FullRecipe = () => {
   }, []);
 
   return (
-    <div>
+    <div className="bg-sky-950 text-white">
       <h1>{data.title}</h1>
-      <div className="flex">
-        <div className="w-50  ">
+      <div className="grid grid-cols-2  h-1/2">
+        <div className="max-h-96 ">
           <img
             alt="content"
-            className=" h-50  w-full"
+            className=" w-full h-full "
             src={`http://localhost:3000/${data.src}`}
           />
         </div>
-        <div className="w-50 bg-sky-950 h-50 p-3">
+        <div className=" bg-sky-950 h-50 p-3">
           <h3 className="text-white uppercase">description</h3>
           <h6 className="text-white uppercase"> {data.description}</h6>
           <h3 className="text-white">ingredients</h3>
@@ -38,6 +38,19 @@ const FullRecipe = () => {
               </div>
             ))}
         </div>
+      </div>
+      <div>
+        <h3>ingredients Sizes</h3>
+        {data.ingredients &&
+          data.ingredients.map((ingredientObj, index) => (
+            <p className="text-white mb-0" key={index}>
+              {ingredientObj.ingredient}: {ingredientObj.description}
+            </p>
+          ))}
+      </div>
+      <div>
+        <h3>Direction</h3>
+        <p>{`${data.direction}`}</p>
       </div>
     </div>
   );
